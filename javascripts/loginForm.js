31c38d879349cbe221206c5ec1205197945a6614
revold1985 <jonathan_drolet@hotmail.com>
2013-12-08 02:13:56 -0500

    modif dans contact

--- javascripts/loginForm.js
+++ javascripts/loginForm.js
@@ -5,12 +5,12 @@ window.onload = function() {
         var poste = sessionStorage.poste;
         document.getElementById(poste).style.display = "block";
     } else {
-        var newHTML = '<h2>Connectez-vous</h2><form id="login"  method="post" action="" name ="formulaire"  onsubmit="return validerFormulaire ();"><input id="codeMS" type="text" name="username" class="placeholder" placeholder="Code MS"><input id="passW" type="password" name="password" class="placeholder" placeholder="Mot de passe"><p class="radio"><input  type="radio" name="choix" id="etudiant" value = 0 checked/>Étudiant<input type="radio" name="choix" id="enseignant" value= 1 />Enseignant<input type="radio" name="choix" id="demonstateur" value= 2 />Démonstrateur </p><input type="submit" value="Valider"  ></form>';
+        var newHTML = '<h2>Connectez-vous</h2><form id="login"  method="post" action="" name ="formulaire"  onsubmit="return validerFormulaire();"><input id="codeMS" type="text" name="username" class="placeholder" placeholder="Code MS"><input id="passW" type="password" name="password" class="placeholder" placeholder="Mot de passe"><p class="radio"><input  type="radio" name="choix" id="etudiant" value = 0 checked/>Étudiant<input type="radio" name="choix" id="enseignant" value= 1 />Enseignant<input type="radio" name="choix" id="demonstateur" value= 2 />Démonstrateur </p><input type="submit" value="Valider"  ></form>';
         document.getElementById('connected').innerHTML = newHTML;
     }
 };
 function deconnection() {
-    var decon = '</br></br></br></br></br><marquee behavior="scroll" direction="right" scrollamount="20"><img src="/images/airplane.svg" width="120" height="100" alt="smile" /><p>Vous êtes maintenant déconnecté !</p></marquee>'
+    var decon = '</br></br></br></br></br><marquee behavior="scroll" direction="right" scrollamount="20"><img src="images/airplane.svg" width="120" height="100" alt="smile" /><p>Vous êtes maintenant déconnecté !</p></marquee>'
     document.body.innerHTML = decon;
     setTimeout(function() {
         sessionStorage.clear();
